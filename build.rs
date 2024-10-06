a5abc1e12fac0a05c6672a827b288132c4a4e3b1
Wyatt Austin <wyatt@tarvos.local>
2024-10-05 22:07:15 -0400

    update

--- build.rs
+++ build.rs
@@ -1,33 +1,33 @@
 fn main() {
+    if std::env::var("CARGO_FEATURE_FFTW3").is_ok() {
+        let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-
-    let c_file = "src/fftwf_exec.c";
-    let obj = "fftwf_exec";
-
-    if target_os == "linux" {
-        // lets assume that clang can find these libraries on linux :p
-        cc::Build::new()
-        .file(c_file)
-        .compile(obj);
-
-        println!("cargo:rustc-link-lib=fftw3f");
-        println!("cargo:rustc-link-lib=fftw3f_threads");
-        println!("cargo:rustc-link-lib=gomp");  // OpenMP support
-
-    }else if target_os == "macos" {
-        // hard-coded include paths for mac
-        cc::Build::new()
-        .file(c_file)
-        .include("/usr/local/opt/fftw/include")
-        .include("/usr/local/opt/libomp/include")
-        .compile(obj);
-
-        println!("cargo:rustc-link-search=native=/usr/local/opt/fftw/lib");
-        println!("cargo:rustc-link-lib=static=fftw3f");
-        println!("cargo:rustc-link-lib=static=fftw3f_threads");
-        println!("cargo:rustc-link-search=native=/usr/local/opt/libomp/lib");
-        println!("cargo:rustc-link-lib=static=omp")
+        let c_file = "src/fftwf_exec.c";
+        let obj = "fftwf_exec";
+    
+        if target_os == "linux" {
+            // lets assume that clang can find these libraries on linux :p
+            cc::Build::new()
+            .file(c_file)
+            .compile(obj);
+    
+            println!("cargo:rustc-link-lib=fftw3f");
+            println!("cargo:rustc-link-lib=fftw3f_threads");
+            println!("cargo:rustc-link-lib=gomp");  // OpenMP support
+    
+        }else if target_os == "macos" {
+            // hard-coded include paths for mac
+            cc::Build::new()
+            .file(c_file)
+            .include("/usr/local/opt/fftw/include")
+            .include("/usr/local/opt/libomp/include")
+            .compile(obj);
+    
+            println!("cargo:rustc-link-search=native=/usr/local/opt/fftw/lib");
+            println!("cargo:rustc-link-lib=static=fftw3f");
+            println!("cargo:rustc-link-lib=static=fftw3f_threads");
+            println!("cargo:rustc-link-search=native=/usr/local/opt/libomp/lib");
+            println!("cargo:rustc-link-lib=static=omp")
+        }
     }
-
 }
